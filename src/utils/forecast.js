50e4af8a8358ef891b91f58fec7a2355bc369200
Alfred De La Costa <ade-la-costa@csumb.edu>
2019-12-29 12:17:40 -0800

    Added new feature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             // If the error propery exists, we know something has gone wrong.
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.apparentTemperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain.");
+            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.apparentTemperature + " degrees out. The high today is: " + body.daily.data[0].temperatureHigh + ", with a low of: " + body.dailt.data[0].temperatureLow + "." + " There is a " + body.currently.precipProbability + "% chance of rain.");
         }
     })
 }
